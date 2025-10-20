f5e1b660c1c118321563cd9ef70088b62818f405
Erdem <erdemyy13@gmail.com>
2025-10-20 11:09:58 +0300

    Delete MinHeapInterface.java

--- MinHeapInterface.java
+++
@@ -1,8 +0,0 @@
-public interface MinHeapInterface<T extends Comparable<? super T>> {
-    void add(T newEntry);
-    T removeMin();
-    T getMin();
-    boolean isEmpty();
-    int getSize();
-    void clear();
-}
